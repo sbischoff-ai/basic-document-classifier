913050b199c9997d2f3768566513231d1b17b29f
Bischoff, Silas <SBischoff@brockhaus-ag.de>
2019-08-09 12:01:40 +0200

    Add test

--- tests/test_document_classifier.py
+++ tests/test_document_classifier.py
@@ -1,5 +1,9 @@
 from document_classifier import __version__
-
+from document_classifier import CNN
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_cnn_instance():
+    cnn = CNN(3)
+    assert not cnn.is_binary
